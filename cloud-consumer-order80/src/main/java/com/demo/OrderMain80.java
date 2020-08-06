8ba69a70d5cd003327c649d2570c7e7952f17b13
d_fei <xpf14561456@163.com>
2020-08-06 14:54:25 +0800

    修改默认ribbon的轮询算法为随机算法

--- cloud-consumer-order80/src/main/java/com/demo/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/demo/OrderMain80.java
@@ -1,11 +1,15 @@
 package com.demo;
 
+import com.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//负载均衡算法由默认的轮询换位随机
+//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80 {
 
     public static void main(String[] args) {
