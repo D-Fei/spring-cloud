4c88d9346fc3dbf0268d7a979fbcfc19056ca150
d_fei <xpf14561456@163.com>
2020-08-07 00:13:21 +0800

    gateway相关操作

---
+++ cloud-gateway-gateway9527/src/test/java/com/demo/T2.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import java.time.ZonedDateTime;
+
+public class T2 {
+    public static void main(String[] args) {
+        // 默认时区
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
