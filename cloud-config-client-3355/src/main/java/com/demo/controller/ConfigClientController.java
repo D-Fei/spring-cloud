59dcb257cb3287072ee99b78adb14d6e187b0e03
d_fei <xpf14561456@163.com>
2020-08-09 00:21:48 +0800

    实现客户端动态跟随修改配置信息

--- cloud-config-client-3355/src/main/java/com/demo/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/demo/controller/ConfigClientController.java
@@ -1,10 +1,15 @@
 package com.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@RefreshScope  加入这个注解后，本类下@Value取值为null，并且不会报错
+@Scope(scopeName="refresh")  //改为这个注解解决问题
+//每当修改GitHub中的配置文件后，需要运维者手动cmd curl -X POST "http://localhost:3355/actuator/refresh"命令才能刷新内容
 public class ConfigClientController {
 
     @Value("${config.info}")
@@ -12,6 +17,7 @@ public class ConfigClientController {
 
     @GetMapping(value = "/configInfo")
     private String getConfigInfo() {
+
         return configInfo;
     }
 
